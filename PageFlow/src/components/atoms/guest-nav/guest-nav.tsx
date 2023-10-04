3db2a5b2d6e9a3ff031fa05e6e8eacf6e7badec2
namyus <106088097+namyus@users.noreply.github.com>
2023-10-04 16:31:49 +0700

    Update guest-nav.tsx

--- PageFlow/src/components/atoms/guest-nav/guest-nav.tsx
+++ PageFlow/src/components/atoms/guest-nav/guest-nav.tsx
@@ -5,7 +5,7 @@ function GuestNav() {
         <>
             <ul>
                 <Link to='/'><li><a href="/">Home</a></li></Link>
-                <Link to='/about'><li><a href="/about">About</a></li></Link>
+                <Link to='/donate'><li><a href="/donate">Donate</a></li></Link>
                 <Link to='/contact'><li><a href="/contact">Contact</a></li></Link>
                 <Link to='/login'><li><a href="/login">Login</a></li></Link>
             </ul>
